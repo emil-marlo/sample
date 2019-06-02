086c171c3655c94d596f69f0d92054510d1bf1fd
emil-marlo <zenlord5.er@gmail.com>
2019-06-02 18:10:48 +0800

    Add NodeJS Express and EJS to the PERS Sample

---
+++ app.js
@@ -0,0 +1,40 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+var user = "";
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/dashboard", function(req,res){
+  res.render('dashboard', {
+    user: user
+  });
+});
+
+//Post this when user tries to login
+app.post("/dashboard", function(req, res){
+  user = req.body.txtUname;
+
+  if (req.body.txtUname === req.body.txtPwd){
+    res.redirect("/dashboard");
+  }else{
+    console.log("Inputs do not match!");
+  }
+
+});
+
+app.listen(3001, function(){
+  console.log("Server is running at port 3001.");
+});
